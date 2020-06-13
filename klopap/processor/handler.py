450b6b9d6824903b130a208d1af445f90b2349ad
Bill X <billxs@hotmail.com>
2020-06-13 16:51:49 +0300

    Initial commit

---
+++ klopap/processor/handler.py
@@ -0,0 +1,35 @@
+class LtrTransactionHandler(TransactionHandler):
+    def __init__(self, namespace_prefix):
+        self._namespace_prefix = namespace_prefix
+
+    @property
+    def family_name(self):
+        return 'ltr'
+
+    @property
+    def family_versions(self):
+        return ['1.0']
+
+    @property
+    def namespaces(self):
+        return [self._namespace_prefix]
+
+    def apply(self, transaction, context):
+        #to complete
+        header = transaction.header
+        signer = header.signer_public_key
+
+        ltr_payload = LotteryPayload.from_bytes(transaction.payload)
+        ltr_state = LotteryState(context)
+
+        if ltr_payload.action == "create":
+            #...
+            if ltr_state.get_lottery(ltr_payload) is not None:
+                raise InvalidTransaction(
+            'Invalid action: You have already play: {}'.format(
+                ltr_payload.numbers))
+
+        elif ltr_payload.action == "validate":
+            #...
+        else:
+            raise InvalidTransaction('Unhandled action: {}'.format(ltr_payload.action))
---
+++ klopap/processor/main.py
@@ -0,0 +1,13 @@
+from sawtooth_sdk.processor.core import TransactionProcessor
+from sawtooth_xo.processor.handler import LotteryTransactionHandler
+
+def main():
+    # In docker, the url would be the validator's container name with
+    # port 4004
+    processor = TransactionProcessor(url='tcp://sawtooth-validator-default-1:4004')
+
+    handler = LotteryTransactionHandler()
+
+    processor.add_handler(handler)
+
+    processor.start()
